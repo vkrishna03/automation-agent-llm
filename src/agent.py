6271549d35403a66edbee7636c31a530e7419e3f
Your Name <example@gmail.com>
2025-02-15 23:20:41 +0530

    minor changes

--- src/agent.py
+++ src/agent.py
@@ -3,6 +3,8 @@ from agno.models.groq import Groq
 from agno.models.openai.chat import OpenAIChat
 from agno.tools.shell import ShellTools
 from phi.tools.sql import SQLTools
+from phi.tools.file import FileTools
+from phi.tools.website import WebsiteTools
 from tools import APIData
 import json
 import os
@@ -35,7 +37,7 @@ def automator_agent(command: str, ):
         name="Automator Agent",
         model = Groq(id="llama-3.3-70b-versatile"),
         # model= Groq(id="mixtral-8x7b-32768"),
-        tools=[ShellTools(), APIData(),SQLTools(db_url=db_url)],
+        tools=[ShellTools(), FileTools(), APIData(),SQLTools(db_url=db_url), WebsiteTools()],
         instructions=["""
         Anytime you are asked to download or generate data from a script 
         make sure you download it in current directory. Python scripts are to be stored in scripts folder. 
@@ -44,10 +46,12 @@ def automator_agent(command: str, ):
         """
             - If asked to write information in a separate file and it does not exist, create it.
             - If asked to clone a repo, make a repo dir and inside it clone it with repo name as folder.
+            - If asked to scrape, use tools given and print scrapped data in tabular format
         """
         """
             Base working directory is: {base_dir}
-        """
+        """,
+        "Use FileTools for performing file operations."
         ],
         show_tool_calls=True
     )
